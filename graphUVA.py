3d56943a0f8b18be05e73e5268410f3564d48626
jasdev <js2wt@virginia.edu>
2013-11-23 16:49:13 -0500

    call executable

--- graphUVA.py
+++ graphUVA.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import subprocess
+import os
+import sys
 
 app = Flask(__name__)
-
+os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 @app.route('/')
 def index():
-    return 'da endpointz'
-
+    p = subprocess.Popen(["./main", "jasdev"], stdout=subprocess.PIPE)
+    return p.communicate()[0]
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
