46053f6d5f5663bef7bbafcae82b5c5207232f5a
Nishant Shukla <nick722@gmail.com>
2013-11-25 21:43:13 -0500

    refactoring

--- main.hs
+++ main.hs
@@ -6,14 +6,11 @@ import Data.Tree.NTree.TypeDefs
 import System.Environment 
 import Text.JSON
 
--- search for "zacho" fails w/
--- main: Prelude.(!!): index too large
-
 data Person = Person 
               { firstName :: String
               , lastName :: String
               , email :: String
-              , other :: [(String, String)] -- grad status, department, phonenumber, etc
+              , other :: [(String, String)]
               } deriving (Show, Eq)
 
 instance JSON Person where 
@@ -22,25 +19,16 @@ instance JSON Person where
                     , ("email", jstr $ email p)
                     , ("other", jobj $ map toJSValTuple (other p)) ]
 
-toJSValTuple (a, b) = (a, jstr b)
-
-jstr :: String -> JSValue
-jstr = JSString . toJSString
-
-jobj :: [(String,JSValue)] -> JSValue
-jobj = JSObject . toJSObject
-
 data SearchResultErr = NoResultsErr | TooManyResultsErr deriving (Show, Eq)
 instance JSON SearchResultErr where 
   showJSON NoResultsErr = jobj [("err", jstr "No Results")]
   showJSON TooManyResultsErr = jobj [("err", jstr "Too Many Results")]
 
-
 type SearchResult = Either SearchResultErr [Person]
 
--- ./main "abba" # searches only "abba"
--- ./main "b" "c" # searches between "b" and "c"
--- ./main # searches everything
+-- ./main "abba"   # searches only "abba"
+-- ./main "b" "c"  # searches between "b" and "c"
+-- ./main          # searches everything
 main = do
   args <- getArgs
   doWork args
@@ -95,9 +83,9 @@ scanDoc query doc = do
           rows <- runX $ doc //> hasName "tr"
           return $ Right (readTableRows rows)
   
-nextDeepQuery query = query ++ "a"
-
 safeMap f ls n = if length ls >= n then map f $ ls !! n else ""
+
+nextDeepQuery query = query ++ "a"
 
 nextQuery "z" = "{"
 nextQuery query = if last query == 'z'
@@ -126,19 +114,14 @@ getTypeFromTr row = getTextFromTd $ getTreeChildren row !! 9
 getDepartmentFromTr row = getTextFromTd $ getTreeChildren row !! 11
 
 getTextFromTd tree = getText' $ getTreeVal $ head $ getTreeChildren tree
-getLinkTextFromTd tree = if null val
-                         then "" 
-                         else (getText'.getTreeVal.head) val
+getLinkTextFromTd tree = 
+  if null val then "" else (getText'.getTreeVal.head) val
   where val = (getTreeChildren.head.getTreeChildren) tree
 
 
 getTreeVal (NTree a b) = a
 getTreeChildren (NTree a b) = b
 getText' (XText a) = a
-
-trim :: String -> String
-trim = f . f
-   where f = reverse . dropWhile isSpace
 
 uvaRequest :: String -> Request_String
 uvaRequest query = Request { 
@@ -150,3 +133,15 @@ uvaRequest query = Request {
   , rqBody = body
   }
   where body = "whitepages=" ++ query
+
+toJSValTuple (a, b) = (a, jstr b)
+
+jstr :: String -> JSValue
+jstr = JSString . toJSString
+
+jobj :: [(String,JSValue)] -> JSValue
+jobj = JSObject . toJSObject
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
