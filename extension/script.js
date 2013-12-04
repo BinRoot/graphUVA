7c7cca655d0dcf254033dd088450f1bf513d7657
jasdev <js2wt@virginia.edu>
2013-12-04 14:53:12 -0500

    adding analytis

--- extension/script.js
+++ extension/script.js
@@ -18,3 +18,11 @@ $(document).ready(function(){
         chrome.runtime.sendMessage( { type: "close" });
     });
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://ssl.google-analytics.com/ga.js','ga');
+
+ga('create', 'UA-46216711-1', 'extension.com');
+ga('send', 'pageview');
