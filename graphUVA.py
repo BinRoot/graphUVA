3fec33d035014e33944fcd6dcfb311f77f70f0e5
root <root@ip-10-154-196-139.ec2.internal>
2013-12-04 21:02:56 +0000

    id render

--- graphUVA.py
+++ graphUVA.py
@@ -24,7 +24,7 @@ def query():
                 person["phoneNumber"] = result.get("other").get("phoneNumber")
                 person["status"] = result.get("other").get("status")
                 person["department"] = result.get("other").get("department")
-                person["value"] = result.get("firstName") + " " + result.get("lastName") + " " + person["email"]
+                person["value"] = result.get("firstName") + " " + result.get("lastName") + ("" if person["email"] is None else " " + "(" + person["email"][0:person["email"].find("@")] + ")")
                 person["name"] = result.get("firstName") + " " + result.get("lastName")
                 person["tokens"] = [result.get("firstName"), result.get("lastName"), result.get("email")]
                 transformed.append(person)
