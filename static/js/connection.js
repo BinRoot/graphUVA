c949cd5de1445cce3a414407943f1ec29cced95d
jasdev <js2wt@virginia.edu>
2013-12-05 05:18:26 -0500

    scaffolding for page search

--- static/js/connection.js
+++ static/js/connection.js
@@ -14,13 +14,18 @@ $(document).ready(function(){
     var spinner = new Spinner(opts).spin(target);
 
     $("#load").hide();
+    $(".results").hide();
 
     $(".search-page").keyup(function (e) {
         if (e.keyCode == 13) {
             $(".search-page").prop('disabled', true);
+            $(".results").hide();
             $("#load").show();
+            setTimeout(function(){
+                $("#load").hide();
+                $(".results").show();
+                $(".search-page").prop('disabled', false);
+            },3000);
         }
     });
-
-
 });
